d4f91d0e329b673882cdac8fee211a98d1aa5242
Johannes <Johannes@Jojo-Desktop>
2017-06-10 15:08:00 +0200

    RSA modul tested

--- ADP11/src/rsa/Krypt.java
+++ ADP11/src/rsa/Krypt.java
@@ -8,21 +8,34 @@ import aufg2_primzahlen.Primzahlsuche;
 
 public class Krypt
 {
-	private int e, n;
+	public int e, n, hauptmodul, nebenmodul;
+	public int q,p;
+	private String testString = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. \n"
+			+ "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem."
+			+ "\nNulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.";
 	public void genererateKeys()
 	{
-		int p = getPrim(), q = getPrim();
-		int hauptmodul = p * q, nebenmodul = (p - 1) * (q - 1);
+		p = getPrim(); q = getPrim();
+		hauptmodul = p * q;
+		nebenmodul = (p - 1) * (q - 1);
 		e = (int) (Math.random() * nebenmodul);
 		if (e <= 1)
 		{
 			e = 2;
 		}
-		while(ExtEuklid.extEuklid(e, nebenmodul) != 1)
+		while (ExtEuklid.extEuklid(e, nebenmodul) != 1)
 		{
 			e++;
 		}
 		n = ExtEuklid.d;
+		if(n<0)
+		{
+			genererateKeys();
+		}
+		else if(!entschluesseln(verschluesseln(testString)).equals(testString))
+		{
+			genererateKeys();
+		}
 	}
 
 	private int getPrim()
@@ -32,7 +45,7 @@ public class Krypt
 		{
 			p = (int) (Math.random() * 500);
 		}
-		while (Primzahlsuche.istPrimzahl(p))
+		while (!Primzahlsuche.istPrimzahl(p))
 		{
 			p++;
 			if (p > 500)
@@ -42,5 +55,57 @@ public class Krypt
 
 		}
 		return p;
+	}
+
+	public int[] verschluesseln(String s)
+	{
+		char[] array = new char[s.length()];
+		int[] intarray = new int[s.length()];
+		s.getChars(0, s.length(), array, 0);
+		String eBin = Integer.toBinaryString(e);
+
+		for (int i = 0; i < array.length; i++)
+		{
+			int a = array[i];
+			long res = 1;
+
+			for (int j = 0; j < eBin.length(); j++)
+			{
+				res = res * res;
+				if (eBin.charAt(j) == '1')
+				{
+					res *= a;
+				}
+				res = res % hauptmodul;
+			}
+			intarray[i] = (char) res;
+		}
+
+		return intarray;
+	}
+	
+	public String entschluesseln(int[] s)
+	{
+		String nBin = Integer.toBinaryString(n);
+		char[] array = new char[s.length];
+		
+		for (int i = 0; i < s.length; i++)
+		{
+			int a = s[i];
+			long res = 1;
+
+			for (int j = 0; j < nBin.length(); j++)
+			{
+				res = res * res;
+				if (nBin.charAt(j) == '1')
+				{
+					res *= a;
+				}
+				res = res % hauptmodul;
+			}
+			array[i] = (char) res;
+		}
+		
+		return new String(array);
 	}
 }
